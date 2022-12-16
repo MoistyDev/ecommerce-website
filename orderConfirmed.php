0ee3f5ca8f85da189061d523b236a394fa4c2123
MoistyDev <xaviermecole@hotmail.com>
2022-12-16 16:53:43 -0500

    feat : added form validation and db inserting of info. Backend done.

--- orderConfirmed.php
+++ orderConfirmed.php
@@ -1,20 +1,37 @@
 <?php
+    require_once "includes/functions/database.php";
 /*
-    $db = db_connect();
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-    $country = $_POST['country'];
-    $address = $_POST['address'];
-    $city = $_POST['city'];
-    $postalcode = $_POST['postalcode'];
-
-    $customer = "insert into customer (firstname, lastname, email) values ('{$firstname}', '{$lastname}', '$email')";
-    $order = "insert into order (customer_id, country, street, city, zip_code) values ((select id from customer where firstname = '{$firstname}' and lastname = '{$lastname}'), '{$country}', '{$address}', '{$city}', '{$postalcode}')";
-    db_query($db, $customer);
-    db_query($db, $order);
+    id
+    productName
+    image
+    quantityOfProduct
 */
-    $cart = json_decode($_POST['cart']);
-
-    echo $cart;
+    $db = db_connect();
+    $orderId = $_GET['orderID'];
+    $cart = json_decode($_POST["cart"]);
+    foreach($cart as $product) {
+        $productId = $product->id;
+        $name = $product->name;
+        $price = $product->price;
+        $quantity = $product->quantity;
+        $insertProduct = "insert into order_item (price, name, quantity, order_id, product_id) values ((select price from product where product_id = {$productId}), '{$name}', {$quantity}, {$orderId}, {$productId})";
+        db_query($db, $insertProduct);
+    }
 ?>
+
+<?php require_once "includes/layouts/shared/header.php" ?>
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <a class="navbar-brand text-center" href="/index.php">Kiwi's Supply Shop - Order #<?=$orderId?> Confirmed</a>
+  </div>
+</nav>
+<div class="content">
+    
+</div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
+<?php pg_close($db); ?>

5adf830e6cff78b73e586bb14563e8b799e21d26
MoistyDev <xaviermecole@hotmail.com>
2022-12-17 13:08:22 -0500

    feat : added header icon and updated page name

--- includes/layouts/shared/header.php
+++ includes/layouts/shared/header.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Boutique en ligne</title>
+    <title>Kiwi's Supply Shop</title>
+    <link rel="icon" href="./public/images/brand-logo-small-tr.png">
     <link rel="stylesheet" href="./public/stylesheets/navbar.css">
     <link rel="stylesheet" href="./public/stylesheets/footer.css">
     <link rel="stylesheet" href="./public/stylesheets/styles.css">
